fc78b5a6e03eedd4c7dcd664e2d6b0cc5cc03dd0
Booble97 <36526376+Booble97@users.noreply.github.com>
2018-10-10 17:15:28 +0900

    Add files via upload
    fewf

--- l01gcal-Booble97/quickstart.py
+++ l01gcal-Booble97/quickstart.py
@@ -3,9 +3,10 @@ import datetime
 from googleapiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
+import xlrd
 
 # If modifying these scopes, delete the file token.json.
-SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
+SCOPES = 'https://www.googleapis.com/auth/calendar'
 
 def main():
     """Shows basic usage of the Google Calendar API.
@@ -18,19 +19,43 @@ def main():
         creds = tools.run_flow(flow, store)
     service = build('calendar', 'v3', http=creds.authorize(Http()))
 
-    # Call the Calendar API
-    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    print('Getting the upcoming 10 events')
-    events_result = service.events().list(calendarId='primary', timeMin=now,
-                                        maxResults=10, singleEvents=True,
-                                        orderBy='startTime').execute()
-    events = events_result.get('items', [])
 
-    if not events:
-        print('No upcoming events found.')
-    for event in events:
-        start = event['start'].get('dateTime', event['start'].get('date'))
-        print(start, event['summary'])
+    # Call the Calendar API
+   
+	
+    start = ['08:00', '09:50', '11:40', '14:00', '15:45', '17:30']
+    end = ['09:35', '11:25', '13:15', '15:35', '17:20', '19:05']  
+    book = xlrd.open_workbook('C:\\Users\\Student\\l01gcal-Booble97\\imi2018.xls')
+    sh = book.sheets()[8]
+    for i in range(3,39): 
+        if sh.cell(i, 8).value=="":
+            continue
+        para = sh.cell(i,8).value
+        l_pr = sh.cell(i,9).value
+        room = sh.cell(i,10).value
+    		
+        event = {
+          'summary': para,
+          'location': room,
+          'description': l_pr,
+          'start': {
+            'dateTime': '2018-10-'+str((i-3)//6+1)+'T'+start[(i-3)%6]+':00+09:00',
+            'timeZone': 'Asia/Yakutsk',
+          },
+          'end': {
+            'dateTime': '2018-10-'+str((i-3)//6+1)+'T'+end[(i-3)%6]+':00+09:00',
+            'timeZone': 'Asia/Yakutsk',
+          },
+          'recurrence': [
+            'RRULE:FREQ=WEEKLY;COUNT=12'
+          ],
+          'reminders': {
+    	  
+          }
+        }
+
+        event = service.events().insert(calendarId='primary', body=event).execute()
+        print('Event created: %s' % (event.get('htmlLink')))
 
 if __name__ == '__main__':
     main()
